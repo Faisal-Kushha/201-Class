d6e31e79153c7a890d0712e1340742dc4f4d9deb
Faisal-Kushha <fkushha89@hotmail.com>
2021-06-20 19:23:40 +0300

    new files

---
+++ app.js
@@ -0,0 +1,17 @@
+let questoin1 = prompt('Welcome to my page, are you sleepy?');
+
+
+console.log(questoin1);
+
+let questoin2 = prompt('Would you like to have a cup of coffee?');
+
+console.log(questoin2);
+
+let questoin3 = prompt('How do you prefer your coffee?');
+
+console.log(questoin3);
+
+let questoin4 = prompt('Are you good now?');
+
+console.log(questoin4);
+
