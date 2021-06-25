a4f679c590489b8c38895d3c2dec42c9093edbcc
Faisal-Kushha <fkushha89@hotmail.com>
2021-06-25 17:15:06 +0300

    edits

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 let questoin1 = prompt('Welcome to my page, What is your name?');
-alert ("Hi " + questoin1 + ".")
 console.log(questoin1);
+alert ("Hi " + questoin1 + ".")
+
 
 let questoin2 = prompt('What is your favorite season in the year?');
-alert(questoin2 + ' is my favorite season too!')
 console.log(questoin2);
+alert(questoin2 + ' is my favorite season too!')
+
 
 let questoin3 = prompt('What is your favorite dish in ' + questoin2 + '?');
-alert (questoin3 + ", I like this dish!")
 console.log(questoin3);
+alert (questoin3 + ", I like this dish!")
+
 
 let questoin4 = prompt('How do you spend your time in ' + questoin2 + '?');
-alert (questoin4 + ", I'd prefer to hang out with friends.")
 console.log(questoin4);
+alert (questoin4 + ", I'd prefer to hang out with friends.")
 
 alert('Nice to meet you ' + questoin1 + ', ' + questoin2 + ' is my favorite season. ' + 'I like to eat ' + questoin3 + ' and ' + questoin4 + ' in ' + questoin2 + '.')
